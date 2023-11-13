01fff0fa783676f1bd76f821261a66949c71dbdf
dario <dario.vandenboom@rcor.de>
2023-11-13 10:07:04 +0100

    Changes to pairing algorithm

--- src/pairings.py
+++ src/pairings.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import pandas as pd
+import matplotlib.pyplot as plt
 
 from enum import Enum
 from typing import List, Dict
@@ -23,7 +24,7 @@ class ColorPref(Enum):
         return NotImplemented
 
 
-def get_k_recent_opponents(spreadsheet, players, k=5) -> Dict[str, List[str]]:
+def get_k_recent_opponents(df: pd.DataFrame, players, k=5) -> Dict[str, List[str]]:
     """Extracts the recent k opponents for each players from the spreadsheet.
 
     Args:
@@ -35,11 +36,6 @@ def get_k_recent_opponents(spreadsheet, players, k=5) -> Dict[str, List[str]]:
         Dict[str, List[str]]: Returns a dict containing the list of recent players for each
         player.
     """
-    df, _ = download_as_dataframe(spreadsheet, 'RawData', 'RawData')
-    print(df)
-    df = df.set_index(['ID'])
-    df['Round'] = pd.to_numeric(df['Round'])
-
     df_filtered = df[(df['White'].isin(players) | df['Black'].isin(players))]
     df_sorted = df_filtered.sort_values(by='Start_Date', ascending=False)
 
@@ -58,7 +54,35 @@ def get_k_recent_opponents(spreadsheet, players, k=5) -> Dict[str, List[str]]:
     return recent_opps
 
 
-def generate_pairings(players: List[str], rtgs: Dict[str, float], recent_k_opps: Dict[str, List[str]], col_pref: Dict[str, ColorPref]):
+def get_recent_game(df, player_a, player_b):
+    players = { player_a, player_b }
+    df_filtered = df[(df['White'].isin(players) | df['Black'].isin(players))]
+    df_sorted = df_filtered.sort_values(by='Start_Date', ascending=False)
+
+    if len(df_sorted) == 0:
+        return None
+    
+    return df_sorted['White'].iloc[0], df_sorted['Black'].iloc[0]
+
+
+def plot_pairings_graph(G):
+    edge_labels = {(a, b): weight for a, b, weight in G.edges(data='weight') if weight < 100}
+    node_labels = {node: node for node in G.nodes()}
+
+    pos = nx.circular_layout(G)
+    nx.draw_networkx_nodes(G, pos)
+    nx.draw_networkx_labels(G, pos, labels=node_labels)
+    nx.draw_networkx_edges(G, pos, edgelist=[(a, b) for a, b, weight in G.edges(data='weight') if weight < 100])
+    nx.draw_networkx_edge_labels(
+        G, pos,
+        edge_labels=edge_labels,
+        font_color='black',
+        font_size=5
+    )
+    plt.show()
+
+
+def generate_pairings(df: pd.DataFrame, players: List[str], rtgs: Dict[str, float], col_pref: Dict[str, ColorPref]):
     """Generates pairings for the given players based on their ratings, recent opponents and color
     preferences. It constraints the potential pairings to not include pairings that have recently
     occured.
@@ -66,43 +90,45 @@ def generate_pairings(players: List[str], rtgs: Dict[str, float], recent_k_opps:
     Args:
         players (List[str]): The players to pair.
         rtgs (Dict[str, float]): The player's ratings.
-        recent_k_opps (Dict[str, List[str]]): The player's recent opponents.
         cols_pref (Dict[str, int]): The color preference for each player.
     """
     assert len(players) % 2 == 0, "Number of players can't be odd!"
+
+    recent_k_opps = get_k_recent_opponents(df, players)
+    print(recent_k_opps)
 
     G = nx.Graph()
     G.add_nodes_from(players)
 
     # Connect two players with an edge iff they don't prefer the same color and haven't been matched recently.
     for a, b in combinations(players, 2):
-        col_pref_check = not (col_pref[a] == col_pref[b] and col_pref[a] != ColorPref.NEUTRAL)
-
-        if col_pref_check and b not in recent_k_opps[a]:
+        if b not in recent_k_opps[a] or b not in recent_k_opps[b]:
             G.add_edge(a, b, weight=abs(rtgs[a]-rtgs[b]))
 
     # Compute the pairings with a min weight matching on the created graph.
     pairings = nx.algorithms.matching.min_weight_matching(G)
 
-    output = list()
-
     # pretty print the pairings.
     for a, b in pairings:
-        if col_pref[a] <= col_pref[b]:
-            output.append((a, b))
+        recent_game = get_recent_game(df, a, b)
+
+        if recent_game != None:
+            if recent_game[0] == a:
+                print(f'@{ a } vs @{ b }')
+            else:
+                print(f'@{ b } vs @{ a }')
+        elif col_pref[a] <= col_pref[b]:
             print(f'@{ a } vs @{ b }')
         else:
-            output.append((b, a))
             print(f'@{ b } vs @{ a }')
 
-    return output
+    plot_pairings_graph(G)
 
 
 if __name__ == '__main__':
     spreadsheet = init_gspread_api()
 
     df, _ = download_as_dataframe(spreadsheet, 'Pairing_Maker', 'PairingMaker')
-    df = df
 
     players = [player.lower() for player in df['Player']]
     pwr_rtgs = pd.to_numeric(df['Power Score']) 
@@ -111,13 +137,14 @@ if __name__ == '__main__':
         pd.to_numeric(df['Color Score'])
     )
 
-    opps = get_k_recent_opponents(spreadsheet, players)
-    print(opps)
+    df, _ = download_as_dataframe(spreadsheet, 'RawData', 'RawData')
+    df = df.set_index(['ID'])
+    df['Round'] = pd.to_numeric(df['Round'])
 
     generate_pairings(
+        df,
         players, 
         {player: rtg for player, rtg in zip(players, pwr_rtgs)}, 
-        opps, 
         {player: c for player, c in zip(players, colors)}
     )
 
